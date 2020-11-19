4d06a79d3e8729757423b27ec0e9f373c8c3a643
Francisco de Assis Boldt <fboldt@gmail.com>
2020-11-19 11:28:35 -0300

    video 4

--- usuario/controle.php
+++ usuario/controle.php
@@ -1,17 +1,33 @@
 <?php
 
+class Credencial {
+    private $usuarios;
+    function __construct() {
+        $this->usuarios = array();
+        $this->usuarios['alice'] = '123';
+        $this->usuarios['bruce'] = '234';
+        $this->usuarios['carol'] = '345';
+    }
+    function confereLoginSenha($login, $senha) {
+        $usuarioExiste = array_key_exists($login, $this->usuarios);
+        if ($usuarioExiste) {
+            $senhaConfere = $this->usuarios[$login] == $senha;
+        }
+        return $usuarioExiste && $senhaConfere;
+    }
+}
+
 class Usuario {
+    private $credencial;
     function __construct() {
         session_start();
+        $this->credencial = new Credencial();
     }
     function getLogin() {
         return $_SESSION['login'];
     }
-    private function confereLoginSenha($login, $senha) {
-        return $login == 'alice' && $senha == '123';
-    }
     function login($login, $senha) {
-        if ($this->confereLoginSenha($login, $senha)) {
+        if ($this->credencial->confereLoginSenha($login, $senha)) {
             $_SESSION["login"] = $login;
         }
     }
