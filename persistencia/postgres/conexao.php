2f07246a6ffb02a2aef8d457a77b64abcc3ae49f
Francisco de Assis Boldt <fboldt@gmail.com>
2021-02-23 15:08:07 -0300

    remove unused dependences

--- persistencia/postgres/conexao.php
+++ persistencia/postgres/conexao.php
@@ -1,13 +1,23 @@
 <?php
-// function getConexao() {
-//     $hostname = 'localhost';
-//     $database = 'tutorialphp';
-//     $username = 'francisco';
-//     $password = 'francisco';
-//     $connstring = "host=$hostname dbname=$database user=$username password=$password";
-//     return pg_connect($connstring);
-// }
+
+function isLocalhost($whitelist = ['127.0.0.1', '::1']) {
+    return in_array($_SERVER['REMOTE_ADDR'], $whitelist);
+}
+
+function getLocalhostConexao() {
+    $hostname = 'localhost';
+    $database = 'tutorialphp';
+    $username = 'francisco';
+    $password = 'francisco';
+    $connstring = "host=$hostname dbname=$database user=$username password=$password";
+    return pg_connect($connstring);
+}
+
 function getConexao() {
+    if(isLocalhost()) {
+        return getLocalhostConexao();
+    }
     return pg_connect(getenv("DATABASE_URL"));
 }
+
 ?>
